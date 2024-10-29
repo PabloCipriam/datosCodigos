67b4893ee80e5d8957063b4a561f76796bb069e1
PabloCipriam <86128687+PabloCipriam@users.noreply.github.com>
2024-10-28 21:50:44 -0500

    Delete js/datos.js

--- js/datos.js
+++
@@ -1,8 +0,0 @@
-const Productos = JSON.parse(localStorage.getItem('productos')) || []
-
-const datos = () => {
-    const productos = JSON.stringify(Productos)
-    console.log(productos)
-}
-
-datos()
